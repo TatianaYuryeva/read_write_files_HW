d23dd685bdf037f40ba4cf35e71b76c19d7c3120
Tatiana Yuryeva <yuryevat@list.ru>
2021-10-14 17:56:49 +0200

    sort_files updated

--- sort_files.py
+++ sort_files.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from pprint import pprint
 all_f = {}
 file_list = ['1.txt', '2.txt', '3.txt']
-data_list=[]
+
 def read_data(file_list):
     for f in file_list:
         with open (f, encoding='utf-8') as file:
@@ -10,22 +10,22 @@ def read_data(file_list):
             numb_lines_f=len(data)
             name = f
             f_dict = {name:[str(numb_lines_f), data]}
-            data_list.append(data)
             all_f.update(f_dict)
 
 read_data(file_list)
 
-all_f_sort = {}
+def write_sort_files(all_f):
+    sorted_tuples = sorted(all_f.items(), key=lambda item: item[1])
 
-sorted_tuples = sorted(all_f.items(), key=lambda item: item[1])
+    sorted_dict = OrderedDict()
+    for k, v in sorted_tuples:
+        sorted_dict[k] = v
 
-sorted_dict = OrderedDict()
-for k, v in sorted_tuples:
-    sorted_dict[k] = v
+    with open('all_f.txt', 'w', encoding='utf-8') as f:
+        for key, val in sorted_dict.items():
+            f.write(f'{key}\n')
+            for el in val:
+                for elem in el:
+                    f.write(f'{elem.rstrip()}\n')
 
-with open('all_f.txt', 'w', encoding='utf-8') as f:
-    for key, val in sorted_dict.items():
-        f.write(f'{key}\n')
-        for el in val:
-            for elem in el:
-                f.write(f'{elem.rstrip()}\n')
+write_sort_files(all_f)
